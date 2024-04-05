99d376f6f1b65aae29f64308502ba594e250bc59
Andrea Venti Fuentes <117413846+av1155@users.noreply.github.com>
2024-04-05 01:33:22 -0400

    fix: Updated nvim update function

--- scripts/package_updater.sh
+++ scripts/package_updater.sh
@@ -11,100 +11,99 @@ NC='\033[0m' # No Color
 
 # Function to check if a command exists
 command_exists() {
-  command -v "$1" >/dev/null 2>&1
+	command -v "$1" >/dev/null 2>&1
 }
 
 # Function to update Homebrew
 update_homebrew() {
-  echo -e "${BLUE}Updating Homebrew...${NC}"
-  brew update
-  brew upgrade
-  brew cleanup
-  brew autoremove
+	echo -e "${BLUE}Updating Homebrew...${NC}"
+	brew update
+	brew upgrade
+	brew cleanup
+	brew autoremove
 }
 
 # Function to update miniforge
 update_miniforge() {
-  echo -e "${BLUE}Updating miniforge...${NC}"
-  conda update --all -y
+	echo -e "${BLUE}Updating miniforge...${NC}"
+	conda update --all -y
 }
 
 # Function to update Oh My Zsh
 update_omz() {
-  echo -e "${BLUE}Updating Oh My Zsh...${NC}"
-  sh "$HOME/.oh-my-zsh/tools/upgrade.sh"
+	echo -e "${BLUE}Updating Oh My Zsh...${NC}"
+	sh "$HOME/.oh-my-zsh/tools/upgrade.sh"
 }
 
 # Function to update Mac App Store applications
 update_mas() {
-  echo -e "${BLUE}Updating Mac App Store applications...${NC}"
-  mas outdated
-  mas upgrade
+	echo -e "${BLUE}Updating Mac App Store applications...${NC}"
+	mas outdated
+	mas upgrade
 }
 
 # Function to update npm
 update_npm() {
-  echo -e "${BLUE}Updating npm...${NC}"
-  npm update -g
+	echo -e "${BLUE}Updating npm...${NC}"
+	npm update -g
 }
 
 # Function to update AstroNvim
 update_astronvim() {
-  echo -e "${BLUE}Updating AstroNvim...${NC}"
-  nvim --headless "+Lazy! sync" +qa
-  nvim --headless "+TSUpdate" +qa
-  nvim --headless "+MasonUpdateAll" +qa
+	echo -e "${BLUE}Updating AstroNvim...${NC}"
+	nvim --headless "+Lazy sync" +qa
+	nvim --headless "+TSUpdate" +qa
+	nvim --headless "+AstroMasonUpdateAll" +qa
 }
 
 # ==============================================
 
 # Update Homebrew
 if command_exists brew; then
-  update_homebrew
-  echo "" # Add a newline for better readability
+	update_homebrew
+	echo "" # Add a newline for better readability
 else
-  echo -e "${RED}Homebrew not found. Skipping...${NC}"
+	echo -e "${RED}Homebrew not found. Skipping...${NC}"
 fi
 
 # Update miniforge
 if command_exists conda; then
-  update_miniforge
-  echo "" # Add a newline for better readability
+	update_miniforge
+	echo "" # Add a newline for better readability
 else
-  echo -e "${RED}miniforge not found. Skipping...${NC}"
+	echo -e "${RED}miniforge not found. Skipping...${NC}"
 fi
 
 # Update Oh My Zsh
 if [ -d "$HOME/.oh-my-zsh" ]; then
-  update_omz
-  echo "" # Add a newline for better readability
+	update_omz
+	echo "" # Add a newline for better readability
 else
-  echo -e "${RED}Oh My Zsh not found. Skipping...${NC}"
+	echo -e "${RED}Oh My Zsh not found. Skipping...${NC}"
 fi
 
 # Update Mac App Store applications
 if command_exists mas; then
-  update_mas
-  echo "" # Add a newline for better readability
+	update_mas
+	echo "" # Add a newline for better readability
 else
-  echo -e "${RED}Mac App Store CLI not found. Skipping...${NC}"
+	echo -e "${RED}Mac App Store CLI not found. Skipping...${NC}"
 fi
 
 # Update npm
 if command_exists npm; then
-  update_npm
-  echo "" # Add a newline for better readability
+	update_npm
+	echo "" # Add a newline for better readability
 else
-  echo -e "${RED}npm not found. Skipping...${NC}"
+	echo -e "${RED}npm not found. Skipping...${NC}"
 fi
 
 # Update AstroNvim
 if command_exists nvim; then
-  update_astronvim
-  echo "" # Add a newline for better readability
+	update_astronvim
+	echo "" # Add a newline for better readability
 else
-  echo -e "${RED}Neovim not found. Skipping AstroNvim update...${NC}"
+	echo -e "${RED}Neovim not found. Skipping AstroNvim update...${NC}"
 fi
 
 echo -e "${GREEN}All applicable packages and applications updated.${NC}"
-
