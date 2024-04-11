0afcf0af788d3696e0608f184ae6aa72490ca342
Andrea Venti Fuentes <117413846+av1155@users.noreply.github.com>
2024-04-11 00:44:34 -0400

    fix: Updated sha256

--- formula/javaprojectmanager.rb
+++ formula/javaprojectmanager.rb
@@ -3,7 +3,7 @@ class Javaprojectmanager < Formula
   homepage "https://github.com/av1155/scripts"
   url "https://raw.githubusercontent.com/av1155/scripts/main/scripts/JavaProjectManager/JavaProjectManager.zsh"
   version "2.0.0"
-  sha256 "66d325dc9126487618eb8d97e06d297ee5e5f4b92eb278fe064d530f154d9e70"
+  sha256 "1ca9457f2577f68e92a0854a19cb489c0fa7a29c042cedcca1893e78198b9573"
   license "MIT"
 
   depends_on "fzf"
